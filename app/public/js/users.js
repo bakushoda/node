eadfe04379ff9d714b1a957155d8f750d3c0d28b
bakushoda <bshoda.ace@gmail.com>
2026-05-16 18:24:11 +0900

    add

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -4,7 +4,20 @@ const usersModule = (() => {
     return {
         fetchAllUsers: async () => {
             const res = await fetch(BASE_URL)
-            
+            const users = await res.json()
+
+            for (let i = 0; i < users.length; i++) {
+                const user = users[i]
+                const body = `<tr>
+                                <td>${user.id}</td>
+                                <td>${user.name}</td>
+                                <td>${user.profile}</td>
+                                <td>${user.date_of_birth}</td>
+                                <td>${user.create_at}</td>
+                                <td>${user.updated_at}</td>
+                             </tr>`
+                document.getElementById('users-list').insertAdjacentHTML('beforeend', body)
+            }
         }
     }
 })()
